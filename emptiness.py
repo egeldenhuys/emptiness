110cc7046374d594e7737795f71afe58eb3f30fd
RagingGrim <armandconnor.duplooy@gmail.com>
2017-02-09 12:46:27 +0200

    Added code to parse URLS

---
+++ emptiness.py
@@ -0,0 +1,15 @@
+#!/bin/python
+from html.parser import HTMLParser
+
+class courseParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+
+if __name__ == '__main__':
+	parser = courseParser
