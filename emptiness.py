4ef5fd6dea4d645fc0e595fd47615b4583a8cdb8
RagingGrim <armandconnor.duplooy@gmail.com>
2017-02-09 12:54:14 +0200

    Full example of html parser.

--- emptiness.py
+++ emptiness.py
@@ -1,15 +1,34 @@
 #!/bin/python
 from html.parser import HTMLParser
 
-class courseParser(HTMLParser):
+class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+        print("Start tag:", tag)
+        for attr in attrs:
+            print("     attr:", attr)
 
     def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
+        print("End tag  :", tag)
 
     def handle_data(self, data):
-        print("Encountered some data  :", data)
+        print("Data     :", data)
+
+    def handle_comment(self, data):
+        print("Comment  :", data)
+
+    def handle_entityref(self, name):
+        c = chr(name2codepoint[name])
+        print("Named ent:", c)
+
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = chr(int(name[1:], 16))
+        else:
+            c = chr(int(name))
+        print("Num ent  :", c)
+
+    def handle_decl(self, data):
+        print("Decl     :", data)
 
 if __name__ == '__main__':
 	parser = courseParser
