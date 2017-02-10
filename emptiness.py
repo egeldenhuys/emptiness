9fb89f885dd26b530b4cc95427373f06ddc7d13d
RagingGrim <armandconnor.duplooy@gmail.com>
2017-02-10 15:09:28 +0200

    Use current time if no arguments given

--- emptiness.py
+++ emptiness.py
@@ -4,14 +4,27 @@ import argparse
 
 import requests
 import timetable
+import datetime
+import time
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument("-d", "--day", default='', required=True, help="Day to check the timetable on. eg: Thursday")
-	parser.add_argument("-t", "--time", default='', required=True, help="The time the block must be empty (HH:MM (24h))")
+	parser.add_argument("-d", "--day", default='', required=False, help="Day to check the timetable on. eg: Thursday")
+	parser.add_argument("-t", "--time", default='', required=False, help="The time the block must be empty (HH:MM (24h))")
 
 	args = parser.parse_args()
+
+	time = args.time
+	day = args.day
+
+	if args.time == '':
+		time = datetime.datetime.now().strftime("%H:%M")
+
+	if args.day == '':
+		day = datetime.datetime.now().strftime("%A")
+
+	# print('Using ' + day + ' - ' + time)
 
 	htmlRequest = requests.get("http://upnet.up.ac.za/tt/hatfield_timetable.html")
 
@@ -20,7 +33,7 @@ if __name__ == '__main__':
 	# Method 1 ; Elimination
 	venueList = timetable.getVenueList(timeTableObject)
 
-	filteredTimetable = timetable.getFilteredTimetable(args.day, args.time, timeTableObject, venueList)
+	filteredTimetable = timetable.getFilteredTimetable(day, time, timeTableObject, venueList)
 
 	#for el in filteredTimetable:
 	#	print(el.venue)
