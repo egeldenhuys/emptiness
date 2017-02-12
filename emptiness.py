7c3bcbcaf97291b802a5cc92d4ac97726cb8603c
RagingGrim <armandconnor.duplooy@gmail.com>
2017-02-12 16:04:40 +0200

    Cronjob should work fine after it is system linked. Should implement another function that does not split the content. In this case it's already split.

--- emptiness.py
+++ emptiness.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 	parser.add_argument('-d', '--day', default='', required=False, help="Day to check the timetable on. (eg: Thursday)")
 	parser.add_argument('-t', '--time', default='', required=False, help="The time the venue must be empty. (eg: 15:30)")
 	parser.add_argument('--version', action='version', version='%(prog)s {version}'.format(version=__version__))
-                    
+
 	args = parser.parse_args()
 
 	time = args.time
@@ -27,16 +27,18 @@ if __name__ == '__main__':
 		day = datetime.datetime.now().strftime('%A')
 
 
-	htmlRequest = requests.get('http://upnet.up.ac.za/tt/hatfield_timetable.html')
+	#htmlRequest = requests.get('http://upnet.up.ac.za/tt/hatfield_timetable.html')
+	content = ''
+	with open('test.html', 'r') as content_file:
+			content = content_file.read()
 
-	timeTableObject = timetable.getTimetableFromHTML(htmlRequest.text)
+	timeTableObject = timetable.getTimetableFromHTML(content)
 	venueList = timetable.getVenueList(timeTableObject)
 
 	filteredTimetable = timetable.getFilteredTimetable(day, time, timeTableObject)
 	emptyVenues = timetable.getEmptyVenues(filteredTimetable, venueList)
-	
+
 	emptyVenues.sort()
-	
+
 	for venueName in emptyVenues:
 		print(venueName)
-		
